58923876fd9a08dd7043bb90c1e701775cd5a9fc
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-22 18:16:04 +0200

    add dom.js with functions which show the weather data

---
+++ src/dom.js
@@ -0,0 +1,34 @@
+function showCurrentWeather(processedData) {
+  const address = document.querySelector(".location-info h2");
+  const description = document.querySelector(".location-info p");
+  const temperature = document.querySelector(".temp-value");
+  const condition = document.querySelector(".condition");
+  const feelsLike = document.querySelector(".feels-like");
+  const humidity = document.querySelector(".humidity");
+  const wind = document.querySelector(".wind");
+
+  address.textContent = processedData.address;
+  description.textContent = processedData.description;
+  temperature.textContent = `${processedData.currentConditions.temp}°`;
+  condition.textContent = `${processedData.currentConditions.conditions}`;
+  feelsLike.textContent = `${processedData.currentConditions.feelslike}°`;
+  humidity.textContent = `${processedData.currentConditions.humidity}%`;
+  wind.textContent = `${processedData.currentConditions.windspeed}km/h`;
+}
+
+function showForecast(processedData) {
+  const forecastDays = document.querySelectorAll(".forecast-day");
+  forecastDays.forEach((forecastDay, index) => {
+    const day = processedData.days[index];
+
+    if (index === 0) {
+      forecastDay.querySelector(".weekday").textContent = "Today";
+    } else {
+      forecastDay.querySelector(".weekday").textContent = day.weekday;
+    }
+    forecastDay.querySelector(".tempmax").textContent = `${day.tempmax}°`;
+    forecastDay.querySelector(".tempmin").textContent = `${day.tempmin}°`;
+  });
+}
+
+export { showCurrentWeather, showForecast };
