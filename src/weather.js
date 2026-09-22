9189156fa4cacc33e4b0527d795b79e493a236b7
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-22 18:15:06 +0200

    specify the data for the output object

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,19 @@ export function processWeatherData(result) {
   return {
     address: result.address,
     description: result.description,
-    currentConditions: result.currentConditions,
-    days: result.days, 
-  }
+    currentConditions: {
+      temp: result.currentConditions.temp,
+      feelslike: result.currentConditions.feelslike,
+      humidity: result.currentConditions.humidity,
+      windspeed: result.currentConditions.windspeed,
+      conditions: result.currentConditions.conditions,
+    },
+    days: result.days.slice(0, 5).map((day) => ({
+      weekday: new Date(day.datetime).toLocaleDateString("en-US", {
+        weekday: "short",
+      }),
+      tempmax: day.tempmax,
+      tempmin: day.tempmin,
+    })),
+  };
 }
