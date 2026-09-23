b72e0112bdba931beff87fa63c6e8c891f4112b3
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-23 19:59:05 +0200

    form reset after submit

--- src/index.js
+++ src/index.js
@@ -3,46 +3,6 @@ import { fetchWeatherData } from "./api.js";
 import { processWeatherData } from "./weather.js";
 import { showCurrentWeather, showForecast } from "./dom.js";
 
-// const test = {
-//   address: "london",
-//   description: "Similar temperatures continuing with a chance of rain Tuesday.",
-//   currentConditions: {
-//     temp: 76.3,
-//     feelslike: 76.3,
-//     humidity: 55.2,
-//     windspeed: 0.8,
-//     conditions: "Partially cloudy",
-//   },
-//   days: [
-//     {
-//       datetime: "2026-09-21",
-//       tempmax: 77.2,
-//       tempmin: 56.7,
-//     },
-//     {
-//       datetime: "2026-09-22",
-//       tempmax: 72.6,
-//       tempmin: 57.2,
-//     },
-//     {
-//       datetime: "2026-09-23",
-//       tempmax: 68.6,
-//       tempmin: 50.1,
-//     },
-//     {
-//       datetime: "2026-09-24",
-//       tempmax: 74.4,
-//       tempmin: 50.8,
-//     },
-//     {
-//       datetime: "2026-09-25",
-//       tempmax: 66.7,
-//       tempmin: 54,
-//     },
-//   ],
-// };
-
-
 const form = document.querySelector(".search-bar");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -50,7 +10,8 @@ form.addEventListener("submit", async (e) => {
   const location = formData.get("location");
   const fetchedData = await fetchWeatherData(location);
   const processedData = processWeatherData(fetchedData);
-  
+
   showCurrentWeather(processedData);
   showForecast(processedData);
-})
+  form.reset();
+});
