79a689a30c47de699d369fdc496b0d13b199447d
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-24 18:42:03 +0200

    api fetch now metric data

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export async function fetchWeatherData(location) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=XLV78DYTEJAKVH3LNL8CYE8JV`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=XLV78DYTEJAKVH3LNL8CYE8JV`,
     );
     const weatherData = await response.json();
     return weatherData;
